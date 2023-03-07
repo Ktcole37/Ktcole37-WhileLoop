25756f15003e3cf3a00d1fad292f0bcb2bf7371f
Kennedy Cole <colekennedyt@gmail.com>
2023-03-07 04:48:13 +0000

    lab

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -39,8 +39,7 @@ public class Lab {
         while(i<end){
         sum = sum + 1;
         i = i + 1;
-        System.out.println(sum);
-        System.out.println(i);    
+            
     }
         return end;
     }
